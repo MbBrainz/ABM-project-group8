407308ac3f87ff380938df56ccbf64ac5647a092
Maurits Bos <mb.brains@gmail.com>
2022-01-23 10:08:18 +0100

    rewrite utils plot functions

--- util.py
+++ util.py
@@ -8,17 +8,25 @@ def fermi_dirac_graph(d, FERMI_ALPHA, FERMI_B):
     pij = 1 / ( 1 + np.exp(FERMI_ALPHA*(abs(d) - FERMI_B)))
     return pij
 
-distances = np.linspace(0, 10, 100)
-probabilities2 = fermi_dirac_graph(distances, 1, 2)
-probabilities3 = fermi_dirac_graph(distances, 1, 3)
-probabilities4 = fermi_dirac_graph(distances, 1, 4)
+def plot_fermidirac():
+    params = [(1,2),(1,3), (1,4)]
 
+    distances = np.linspace(0, 10, 100)
+    plot_data =[]
+    for param in params:
+        data = {
+            "y": fermi_dirac_graph(distances, param[0], param[1]),
+            "alpha": param[0],
+            "b": param[1]
+            }
+        plot_data.append(data)
 
-# plt.plot(distances, probabilities2, label = "alpha = 1, b = 2")
-# plt.plot(distances, probabilities3, label = "alpha = 1, b = 3")
-# plt.plot(distances, probabilities4, label = "alpha = 1, b = 4")
-# plt.xlabel("Absolute distance in the political belief-space")
-# plt.ylabel("Probability to connect")
-# plt.title("Fermi-Dirac probability to connect to another node based on political distance")
-# plt.legend()
-# plt.show()
+    for data in plot_data:
+        plt.plot(distances, data['y'], label = f"alpha = {data['alpha']}, b = {data['b']}")
+    plt.xlabel("Absolute distance in the political belief-space")
+    plt.ylabel("Probability to connect")
+    plt.title("Fermi-Dirac probability to connect to another node based on political distance")
+    plt.legend()
+    plt.show()
+
+# plot_fermidirac()

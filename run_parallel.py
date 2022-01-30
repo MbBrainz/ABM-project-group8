012becea530abb62083f7ee8dfb5979922d8f308
Maurits Bos <mb.brains@gmail.com>
2022-01-30 13:32:45 +0100

    Automated concat dataframe. This results in just one df per sampleset

--- run_parallel.py
+++ run_parallel.py
@@ -1,14 +1,23 @@
+# This file enables you to run multiple samples of multiple parametersets in parallel
+# Use run_parallel() with your list of parameter sets to run for these parameters
+# use read_dataframe() for reading the results of a parameterset
+#
+# DONT USE FUNCTIONS WICH START WITH UNDERSCORE(_)
+#
 
 import multiprocessing
 import os
-from telnetlib import STATUS
 import pandas as pd
 from tqdm import tqdm
 from polarization.model import CityModel, ModelParams
 
 DATA_DIR = "./dump/data/"
 
-def init_and_start_model(params, dir=DATA_DIR):
+# saving directories:
+sample_dir  = lambda dir, type_str, params_str, sample_nr:  f"{dir}-{type_str}df-{params_str}{sample_nr}.pkl"
+final_dir   = lambda dir, type_str, params_str, nsamples:   f"{dir}{type_str}df-params={params_str}nsamples={nsamples}.pkl"
+
+def _init_and_start_model(params, dir=DATA_DIR):
     """Initilalises model with *params , starts the modelsimulation and stores the data to a .pkl file in data directory
 
     Args:
@@ -31,13 +40,14 @@ def init_and_start_model(params, dir=DATA_DIR):
     model.run_model(param_set, desc=f"core {pos}, sample {sample_id}: {sample_nr}/{nr_samples}", pos=pos, collect_during=False)
 
     agent_df = model.datacollector.get_agent_vars_dataframe()
-    agent_df.insert(0,"Sample", sample_nr)
-    agent_df.to_pickle(f"{dir}_agentdf_{param_set.to_dir()}{sample_nr}.pkl")
-
     model_df = model.datacollector.get_model_vars_dataframe()
-    model_df.insert(0, "Sample", sample_nr) # to be able do distinguish results from specific samples
-    model_df.to_pickle(f"{dir}_modeldf_{param_set.to_dir()}{sample_nr}.pkl")
+    # to be able do distinguish results from specific samples
+    agent_df.insert(0,"Sample", sample_nr)
+    model_df.insert(0, "Sample", sample_nr)
 
+    # write to file
+    agent_df.to_pickle(sample_dir(dir, "agent", param_set.to_dir(),sample_nr))
+    model_df.to_pickle(sample_dir(dir, "model", param_set.to_dir(),sample_nr))
 
 def simulate_parallel(params_list: list[ModelParams], distinct_samples=1):
     """Simulates in parallel for each item in the parameterlist and saves it to the data folder.
@@ -55,43 +65,23 @@ def simulate_parallel(params_list: list[ModelParams], distinct_samples=1):
         params_and_data_list = [(params, x+1, index, distinct_samples) for x in range(distinct_samples)]
         data_and_samples.extend(params_and_data_list)
 
-
     tqdm.set_lock(multiprocessing.RLock())
     pool = multiprocessing.Pool(initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),))
-    pool.map(init_and_start_model, data_and_samples)
+    pool.map(_init_and_start_model, data_and_samples)
 
     pool.close()
     pool.join()
 
-def read_dataframe_for_param_set(params, distinct_samples, dir=DATA_DIR):
-    """Read the dataframe for all the samples of a specific parameter set.
+    _concat_samples(params_list, distinct_samples=distinct_samples)
 
-    Args:
-        params (ModelParams): parameters to get the data for
-        distinct_samples (int): number of samples that you want to collect form data folder
-        dir ([type], optional): Directory in which the data is saved. Defaults to DATA_DIR -> "./dump/data/" .
+def _concat_samples(params_list, distinct_samples, dir=DATA_DIR):
+    for params in params_list:
+        agent_df, model_df = _read_dataframe_for_param_set(params=params, remove_files=True)
 
-    Returns:
-        tuple(Dataframe, Dataframe): [0] agent dataframe, [1] model dataframe
-    """
-    agent_df_list = []
-    model_df_list = []
+        agent_df.to_pickle(final_dir(dir,"agent", params.to_dir(), distinct_samples))
+        model_df.to_pickle(final_dir(dir,"model", params.to_dir(), distinct_samples))
 
-    for i in range(0,distinct_samples):
-        try:
-            sample_df = read_dataframe(params=params, sample_nr=i+1, dir=dir)
-            agent_df_list.append(sample_df[0])
-            model_df_list.append(sample_df[1])
-        except:
-            print(f"\n [WARNING]: requested {distinct_samples} distinct samples, but only {i} found. check the data folder if your data is really there.\n\n")
-            break
-
-    agent_df = pd.concat(agent_df_list)
-    model_df = pd.concat(model_df_list)
-    return agent_df, model_df # with agent dataframe and model dataframe
-
-
-def read_dataframe(params, sample_nr=1, dir=DATA_DIR):
+def _read_dataframe_sample(params, sample_nr=1, dir=DATA_DIR, remove_files=False):
     """Reads dataframe from .pkl file that is created by the simulate_parallel function. It uses *params to see get the matching directory
 
     Args:
@@ -100,10 +90,47 @@ def read_dataframe(params, sample_nr=1, dir=DATA_DIR):
     Returns:
         tuple(Dataframe,Dataframe): agent dataframe[0] and model dataframe[1]
     """
-    agent_dir = f"{dir}_agentdf_{params.to_dir()}{sample_nr}.pkl"
+    agent_dir = sample_dir(dir, "agent", params.to_dir(),sample_nr)
+    model_dir = sample_dir(dir, "model", params.to_dir(),sample_nr)
     agent_df  = pd.read_pickle(agent_dir)
-    model_dir = f"{dir}_modeldf_{params.to_dir()}{sample_nr}.pkl"
     model_df  = pd.read_pickle(model_dir)
+
+    if remove_files:
+        os.remove(agent_dir)
+        os.remove(model_dir)
+
+    return (agent_df, model_df)
+
+def _read_dataframe_for_param_set(params, dir=DATA_DIR, remove_files=False):
+    """Read the dataframe for all the samples of a specific parameter set.
+
+    Args:
+        params (ModelParams): parameters to get the data for
+        dir ([type], optional): Directory in which the data is saved. Defaults to DATA_DIR -> "./dump/data/" .
+
+    Returns:
+        tuple(Dataframe, Dataframe): [0] agent dataframe, [1] model dataframe
+    """
+    agent_df_list = []
+    model_df_list = []
+    i = 0
+    while True:
+        try: # read samples until reader returns FileNotFound Error
+            sample_df = _read_dataframe_sample(params=params, sample_nr=i+1, dir=dir, remove_files=remove_files)
+            agent_df_list.append(sample_df[0])
+            model_df_list.append(sample_df[1])
+            i += 1
+        except FileNotFoundError:
+            print(f"\n\nCollected data for {i} distinct samples.\n\n")
+            break
+
+    agent_df = pd.concat(agent_df_list)
+    model_df = pd.concat(model_df_list)
+    return agent_df, model_df # with agent dataframe and model dataframe
+
+def read_dataframe(params, nsamples, dir=DATA_DIR):
+    agent_df = pd.read_pickle(final_dir(dir, "agent", params.to_dir(), nsamples ))
+    model_df = pd.read_pickle(final_dir(dir, "model", params.to_dir(), nsamples ))
     return (agent_df, model_df)
 
 def example():
@@ -117,13 +144,14 @@ def example():
         # ModelParams(sidelength=10, density=0.9, m_barabasi=2, social_factor=0.8, connections_per_step=5, fermi_alpha=5, fermi_b=3, opinion_max_diff=2, total_steps=10, happiness_threshold=0.6),
     ]
 
-    distinct_samples=2
+    distinct_samples=5
 
     simulate_parallel(params_list, distinct_samples=distinct_samples)
 
     # here the files are read. This should be done separately from simulation, e.g. at visualisation.
-    params = params_list[0]
-    agent_df, model_df = read_dataframe_for_param_set(params, distinct_samples=distinct_samples+1)
+    params = params_list[1]
+
+    agent_df, model_df = read_dataframe(params, nsamples=distinct_samples)
 
     print(model_df)
 

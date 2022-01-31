3d524130b7a74e0160b30b0bfe11e147d9c49911
nholzbach <81358704+nholzbach@users.noreply.github.com>
2022-01-31 15:38:03 +0100

    working ofat

--- dump/ofat.py
+++ dump/ofat.py
@@ -34,9 +34,9 @@ problem = {
 #%%
 #set repitions(compensate for stochasticity), number of steps and amount of distinct values per variable(N. 100 is good for us)
 #total sample size = N * (num_vars+2)  
-replicates = 2
+replicates = 1
 max_steps = 50
-distinct_samples = 10
+distinct_samples = 3
 #%%
 #set output
 #not sure how to connect this to our schedule 
@@ -108,46 +108,3 @@ for param in (['graph_modularity','altieri_entropy_index']):
     plt.show()
 
 # %%
-#plotting single output(only modularity)
-def plot_param_var_conf(ax,df,var,param,i):
-    """
-    Helper function for plot_all_vars. Plots the individual parameter vs
-    variables passed.
-
-    Args:
-        ax: the axis to plot to
-        df: dataframe that holds the data to be plotted
-        var: variables to be taken from the dataframe
-        param: which output variable to plot
-    """
-    x = df.groupby(var).mean().reset_index()[var]
-    y = df.groupby(var).mean()[param]
-
-    replicates = df.groupby(var)[param].count()
-    err = (1.96 * df.groupby(var)[param].std()) / np.sqrt(replicates)
-
-    ax.plot(x,y,c='k')
-    ax.fill_between(x,y-err,y+err)
-
-    ax.set_xlabel(var)
-    ax.set_ylabel(param)
-
-def plot_all_vars(df,param):
-    """
-    Plots the parameters passed vs each of the output variables.
-
-    Args:
-        df: dataframe that holds all data
-        param: the parameter to be plotted
-    """
-    f,axs=plt.subplots(6, figsize=(7,10), sharey=True)
-
-
-
-    for i, var in enumerate(problem['names']):
-        plot_param_var_conf(axs[i], data[var], var, param, i)
-
-for param in (['graph_modularity']):
-    print(f"Param: {param}")
-    plot_all_vars(data, param)
-    plt.show()

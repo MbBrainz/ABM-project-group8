fab22bb79ba565dbfb4c792fdec66a4a145d69a5
nholzbach <81358704+nholzbach@users.noreply.github.com>
2022-01-31 14:09:32 +0100

    addition of entropy

--- dump/ofat.py
+++ dump/ofat.py
@@ -5,11 +5,12 @@
 import os, sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from polarization.model import CityModel, Resident
-from mesa.batchrunner import BatchRunner
+from mesa.batchrunner import BatchRunner, BatchRunnerMP
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from itertools import combinations
+plt.style.use('seaborn')
 
 #from run_parallel import simulate_parallel
 
@@ -27,45 +28,31 @@ problem = {
     'num_vars':6,
     'names':['fermi_alpha','fermi_b', 'social_factor',
     'connections_per_step','opinion_max_diff', 'happiness_threshold'],
-    'bounds':[[2,10],[0,10],[0,1],[1,5],[1,10],[0,1]],
+    'bounds':[[2,10],[0,10],[0,1],[1,5],[0,5],[0,1]],
 }
 
 #%%
 #set repitions(compensate for stochasticity), number of steps and amount of distinct values per variable(N. 100 is good for us)
 #total sample size = N * (num_vars+2)  
 replicates = 2
-max_steps = 10
+max_steps = 50
 distinct_samples = 10
 #%%
 #set output
 #not sure how to connect this to our schedule 
-model_reporters={"Network modularity": lambda m:m.calculate_modularity}
+model_reporters={"graph_modularity": lambda m:m.calculate_modularity(),
+                "altieri_entropy_index": lambda m:m.calculate_a_entropyindex()}
 #how it is stated in the Model class - "graph_modularity": self.calculate_modularity 
 
 data={}
-
-#%%
-#doing what maups said - only for one varying parameter
-
-# fixed = [10,10,0.6,2]
-# for i,var in enumerate(test['names']):
-#     print(var)
-#     default = [1,1,1,1]
-#     varied=np.linspace(*test['bounds'][i], num=distinct_samples)
-#     for option in varied:
-#         param_set=fixed+[option]+default
-#         #i know that .insert() is faster but it doesn't work here (?)
-#         print(param_set)
-
-# #now to make it general:
-#need to make it look through varible params and adjust the defaults 
-
 #%%
 for i,var in enumerate(problem['names']):
     #get bounds for the variable and get <distinct_samples> samples within this space (uniform)
     param_values = np.linspace(*problem['bounds'][i],num=distinct_samples)
+    if var == 'connections_per_step':
+        param_values = np.linspace(*problem['bounds'][i], num=distinct_samples, dtype=int)
 
-    batch = BatchRunner(CityModel,
+    batch = BatchRunnerMP(CityModel,
                         max_steps=max_steps,
                         iterations=replicates,
                         variable_parameters={var:param_values},
@@ -73,10 +60,11 @@ for i,var in enumerate(problem['names']):
                         display_progress=True)
     batch.run_all()
 
-    data[var]=batch.get_agent_vars_dataframe()
-
+    data[var]=batch.get_model_vars_dataframe()
 #%%
-#plotting 
+print(data)
+#%%
+#plotting double output
 def plot_param_var_conf(ax,df,var,param,i):
     """
     Helper function for plot_all_vars. Plots the individual parameter vs
@@ -108,11 +96,58 @@ def plot_all_vars(df,param):
         df: dataframe that holds all data
         param: the parameter to be plotted
     """
-    f,axs=plt.subplots(3, figsize=(7,10))
+    f,axs=plt.subplots(6, figsize=(7,10))
+
 
     for i, var in enumerate(problem['names']):
         plot_param_var_conf(axs[i], data[var], var, param, i)
 
-for param in ('Network modularity'):
+for param in (['graph_modularity','altieri_entropy_index']):
+    print(f"Param: {param}")
+    plot_all_vars(data, param)
+    plt.show()
+
+# %%
+#plotting single output(only modularity)
+def plot_param_var_conf(ax,df,var,param,i):
+    """
+    Helper function for plot_all_vars. Plots the individual parameter vs
+    variables passed.
+
+    Args:
+        ax: the axis to plot to
+        df: dataframe that holds the data to be plotted
+        var: variables to be taken from the dataframe
+        param: which output variable to plot
+    """
+    x = df.groupby(var).mean().reset_index()[var]
+    y = df.groupby(var).mean()[param]
+
+    replicates = df.groupby(var)[param].count()
+    err = (1.96 * df.groupby(var)[param].std()) / np.sqrt(replicates)
+
+    ax.plot(x,y,c='k')
+    ax.fill_between(x,y-err,y+err)
+
+    ax.set_xlabel(var)
+    ax.set_ylabel(param)
+
+def plot_all_vars(df,param):
+    """
+    Plots the parameters passed vs each of the output variables.
+
+    Args:
+        df: dataframe that holds all data
+        param: the parameter to be plotted
+    """
+    f,axs=plt.subplots(6, figsize=(7,10), sharey=True)
+
+
+
+    for i, var in enumerate(problem['names']):
+        plot_param_var_conf(axs[i], data[var], var, param, i)
+
+for param in (['graph_modularity']):
+    print(f"Param: {param}")
     plot_all_vars(data, param)
     plt.show()

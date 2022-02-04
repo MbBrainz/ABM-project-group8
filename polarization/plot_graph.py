9e674dc16c99ce0ac3589040a19a6838f168197f
Maurits Bos <mb.brains@gmail.com>
2022-02-04 17:12:34 +0100

    Corrected the update opinion function and removed the ModelParams variable

--- polarization/plot_graph.py
+++ polarization/plot_graph.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
 
-def create_graph(agent_df, model_df, graph_axes= [], colormap="bwr"):
+def create_graph(agent_df, model_df, graph_axes= [], colormap="bwr", layout=nx.spring_layout):
     first_run_dict = model_df.loc[model_df.index[0],"edges"]
     G_init = nx.from_dict_of_dicts(first_run_dict)
 
@@ -38,15 +38,14 @@ def create_graph(agent_df, model_df, graph_axes= [], colormap="bwr"):
         cbar = fig.colorbar(ScalarMappable(norm=Normalize(0,1), cmap=cmap), orientation='horizontal',label="Opinion", ticks=[0,1])
         cbar.ax.set_xticklabels(['Far left', 'Far right'])
 
-    nx.draw(G_init, ax=graph_axes[0], node_size=100, node_color=color_map_first, width=0.3, edgecolors='k')
-    nx.draw(G_last, ax=graph_axes[1], node_size=100, node_color=color_map_last, width=0.3, edgecolors='k')
+    nx.draw(G_init, ax=graph_axes[0], node_size=20, node_color=color_map_first, width=0.3, edgecolors='k', pos=layout(G_init))
+    nx.draw(G_last, ax=graph_axes[1], node_size=20, node_color=color_map_last, width=0.3, edgecolors='k', pos=layout(G_last))
 
     graph_axes[0].set_title('Initialized')
     graph_axes[1].set_title('Final State')
 
-    plt.show()
 
     # %%
-from util import testagent_df, testmodel_df
-create_graph(testagent_df, testmodel_df)
+# from util import testagent_df, testmodel_df
+# create_graph(testagent_df, testmodel_df)
 # %%

3f570147513f23f7400cad9b3a05d7d90a179584
nholzbach <81358704+nholzbach@users.noreply.github.com>
2022-02-08 22:13:20 +0100

    cleaned

--- sobol_plot.py
+++ sobol_plot.py
@@ -1,4 +1,4 @@
-#%%
+"""This script sets up the sobol Global Sensitivity Analysis, using the data collected and saved in data/sobol"""
 
 from itertools import combinations
 import numpy as np
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from SALib.analyze import sobol
+plt.style.use('seaborn')
 
 problem = {
     'num_vars':5,
@@ -13,39 +14,26 @@ problem = {
     'Max. difference in opinion', 'Happiness Threshold'],
     'bounds':[[0,4],[0,6],[0,1],[0,4],[0,1]],
 }
-# problem = {
-#     'num_vars':5,
-#     'names':['fermi_alpha','fermi_b', 'social_factor',
-#     'opinion_max_diff', 'happiness_threshold'],
-#     'bounds':[[0,4],[0,6],[0,1],[0,4],[0,1]],
-# }
-
+#set up data, loading from all csv files
 DATA_DIR = "./data/sobol/"
 datafiles = [dir for dir in os.listdir(DATA_DIR) if dir.startswith("sobol3")]
 
-print(datafiles)
-# %%
 dataframes = []
 for dir in datafiles:
     dataframes.append(pd.read_csv(f"{DATA_DIR}{dir}"))
 
 sobol_df = pd.concat(dataframes)
-#%%
 sobol_df = sobol_df.drop(columns=["Unnamed: 0"])
-#%%
 
 
-# sobol_df.head()
-#%%
-sobol_df.describe()
-# %%
 # Creating a sortable column so the dataframe is ordered
 sobol_df['indexing'] = sobol_df[['fermi_alpha', 'fermi_b', 'social_factor', 'opinion_max_diff', 'happiness_threshold']].sum(axis=1)
 sobol_id_describe = sobol_df.groupby(by=["indexing"]).describe()
 sobol_df = sobol_df.sort_values("indexing")
-#%% Counting duplicates
-sobol_id_describe["fermi_alpha"]["count"].value_counts()
-# %%
+
+#Counting duplicates
+#sobol_id_describe["fermi_alpha"]["count"].value_counts()
+
 columns = sobol_df.columns
 columns = [x.title().replace("_"," ") for x in columns]
 columns[0] = "Fermi-Dirac alpha"
@@ -55,10 +43,11 @@ columns[3] = "Max. difference in opinion"
 sobol_df.columns = columns
 sobol_df.head()
 
+#SOBOL ANALYZE
 si_resident_modularity = sobol.analyze(problem, sobol_df['Network Modularity'].values[:4438], print_to_console=True, calc_second_order=False)
 si_resident_entropy = sobol.analyze(problem, sobol_df['Altieri Entropy Index'].values[:4438], print_to_console=True, calc_second_order=False)
 
-#%%
+
 def plot_index(s, params, i, ax=None, title=''):
     """
     Creates a plot for Sobol sensitivity analysis that shows the contributions
@@ -86,7 +75,6 @@ def plot_index(s, params, i, ax=None, title=''):
 
     #set some aesthetics here
     l=len(indices)
-    #edit this when we know what it looks like
     # plt.ylim([-0.2,l-1+0.2])
     # plt.xlim(-0.1, 1.1)
 
@@ -94,18 +82,16 @@ def plot_index(s, params, i, ax=None, title=''):
         fig, ax = plt.subplots()
     ax.set_yticks(range(l), params)
     ax.set(title=title)
-    # ax.set_xticks()
     ax.errorbar(indices, range(l), xerr=errors, linestyle='None', marker='o', capsize=2)
     ax.axvline(0,c='k')
     plt.tight_layout()
     # plt.savefig("filename.png")
-# %%
-plt.style.use('seaborn')
+
+#Plotting first and total order SA for Modularity and Entropy
 fig, ax = plt.subplots(2,2, figsize=(9,5), sharey=True, sharex=True)
-# ax[0][0].set(supylabel="N|na")
 plot_index(si_resident_modularity , problem["names"],   i="1",ax=ax[0][0],title= "1st Order Sensitivity\nModularity")
 plot_index(si_resident_entropy , problem["names"],      i="1",ax=ax[1][0],title= "Entropy")
 plot_index(si_resident_modularity , problem["names"],   i="T",ax=ax[0][1],title= "Total S\nModularity")
 plot_index(si_resident_entropy , problem["names"],      i="T",ax=ax[1][1],title= "Entropy")
-plt.savefig("figures/GSA.svg")
-# %%
+#plt.savefig("figures/GSA.svg")
+
